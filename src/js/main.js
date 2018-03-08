1229d092f8be70f755dc1a29d647c181e78fff78
romzych <rblondel23@gmail.com>
2018-03-08 12:07:14 +0100

    oui

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,11 @@
 var options = {
     controls: true,
     autoplay: false,
-    playbackRate: 4,
     controlBar: {
         volumePanel: {inline: false}
-    }
+    },
+    aspectRatio:"16:9",
+    playbackRates: [0.5, 1, 1.5, 2]
 };
 
 var player = videojs('player_id_tag', options, function () {
@@ -23,14 +24,10 @@ var player = videojs('player_id_tag', options, function () {
     // Paramètres pour configurer le plugin, mais il faut l'installer avant !
     this.watermark({
         image: 'http://tous-logos.com/wp-content/uploads/2017/03/YouTube-logo-500x255.png',
-        url: 'https://www.youtube.com/watch?v=nZMrbGyfx0U'
+        url: '#'
     });
 
     // En se basant sur la configuration des paramètres du plugin Watermark, établissez les paramètres du plugin Hotkeys ci-dessous (en consultant la doc du plugin bien sûr)
-
-});
-
-var player = videojs('player_id_tag').ready(function() {
     this.hotkeys({
         volumeStep: 0.2,
         seekStep: 3,
